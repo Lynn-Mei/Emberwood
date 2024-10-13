f0b59f9d5567ca60a454330f52df7be46065db38
Hugo-Mrula <hugo.murura@gmail.com>
2024-10-13 18:38:49 +0200

    Adding game controller

---
+++ Emberwood/Display/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emberwood.Display
+{
+    public class GameController
+    {
+        private static GameController instance = null;
+        private Game1 game;
+        public Game1 Game { get { return this.game; } set { this.game = value; } }
+        private GameController() { }
+        public static GameController Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new GameController();
+                }
+                return instance;
+            }
+        }
+
+    }
+}
