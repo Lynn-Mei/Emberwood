c3f492d8e0393997870d5c801cdad128418826cf
Hugo-Mrula <hugo.murura@gmail.com>
2024-10-13 18:37:21 +0200

    Base game

---
+++ Emberwood/Game1.cs
@@ -0,0 +1,66 @@
+﻿using Emberwood.Display;
+using Emberwood.Environment;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Emberwood
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Camera _camera;
+        private Avatar _avatar;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            GameController.Instance.Game = this;
+            this._avatar = new Avatar();
+            this._camera = new Camera();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            this._avatar.UnloadBeyoundScope(_camera);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+            foreach(IElement e in _avatar.GetInCameraSight(_camera))
+            {
+                e.Draw(_spriteBatch);
+            }
+
+
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
