c5a73d341fe473d302787e6f5a0675b5a501e0e8
Hugo-Mrula <hugo.murura@gmail.com>
2024-10-13 18:38:37 +0200

    Adding Ielement and its avatar

---
+++ Emberwood/Environment/Avatar.cs
@@ -0,0 +1,44 @@
+﻿using Emberwood.Display;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emberwood.Environment
+{
+    /// <summary>
+    /// Master of all elements
+    /// </summary>
+    public class Avatar
+    {
+        const int avatarScope = 10; 
+        private List<IElement> elements = new List<IElement>();
+        public List<IElement> Elements { get; }
+
+        public void UnloadBeyoundScope(Camera c) { 
+            foreach (IElement element in elements)
+            {
+                Coordinate coord = element.TileCoordinate.GetPhysicalCoordinate();
+                if (c.IsInScope(coord, avatarScope)) { 
+                    Elements.Remove(element);
+                }
+            }
+        }
+
+        public List<IElement> GetInCameraSight(Camera c)
+        {
+            List<IElement> viewedElements = new List<IElement>();
+            foreach (IElement element in elements)
+            {
+                Coordinate coord = element.TileCoordinate.GetPhysicalCoordinate();
+                if (c.IsInScope(coord))
+                {
+                    viewedElements.Add(element);
+                }
+            }
+            return viewedElements;
+        }
+
+    }
+}
---
+++ Emberwood/Environment/IElement.cs
@@ -0,0 +1,20 @@
+﻿using Emberwood.Display;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emberwood.Environment
+{
+    public abstract class IElement
+    {
+        private TileCoordinate coordinate;
+
+        public TileCoordinate TileCoordinate { get { return this.coordinate; } }
+
+        public abstract bool IsGround { get; }
+        public abstract void Draw(SpriteBatch _spriteBatch); 
+    }
+}
